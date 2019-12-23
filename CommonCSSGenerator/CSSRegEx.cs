44fbcf8234dae984c560b1d7a3d1901912490d3f
Andrea Carratta <andrea.carratta.dev@outlook.it>
2019-12-23 14:03:25 +0100

    CommonCSSGenerator

---
+++ CommonCSSGenerator/CSSRegEx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CommonCSSGenerator
+{
+    // RegEx from : https://www.codeproject.com/Articles/335850/CSSParser
+    class CSSRegEx
+    {
+
+        private const string CSSGroups = @"(?<selector>(?:(?:[^,{]+),?)*?)\{(?:(?<name>[^}:]+):?(?<value>[^};]+);?)*?\}";
+
+        private const string CSSComments = @"(?<!"")\/\*.+?\*\/(?!"")";
+
+
+        private static Regex _groups = null;
+        public Regex Groups
+        {
+            get
+            {
+
+                if (_groups == null)
+                {
+                    _groups = new Regex(CSSGroups, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                }
+                return _groups;
+            }
+        }
+
+        private static Regex _comments = null;
+        public Regex Comments
+        {
+            get
+            {
+
+                if (_comments == null)
+                {
+                    _comments = new Regex(CSSComments, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+                }
+                return _comments;
+            }
+        }
+
+    }
+}
---
+++ CommonCSSGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CommonCSSGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Insert the Input Folder:");
+            string source = Console.ReadLine();
+
+            Console.WriteLine("Please Insert the Output Folder:");
+            string output = Console.ReadLine();
+        }
+    }
+}
